f8c0262578f9e2e10a379554c240099c7ae4fb90
george <heohii.pustovit@ava.codes>
2022-11-18 10:06:12 +0200

    Write text for experience

--- src/components/Experience/utils.ts
+++ src/components/Experience/utils.ts
@@ -1,24 +1,26 @@
 export const workExperience = [
   {
     title: 'Full stack developer at Nection.io',
-    description: 'Dealing with lots of tricky tasks',
+    description:
+      'Working as the lead developer at a fast-growing startup on mobile application that helps people to manage their relationships. I deal with all kinds of tasks on the frontend, backend, set up servers and help new developers to onboard on the project',
     date: '11.2021 - present',
   },
   {
-    title: 'React/Node.js at AVA.codes',
-    description: 'Dealing with lots of tricky tasks',
+    title: 'React/Node.js developer at AVA.codes',
+    description:
+      'AVA.codes is an outstaff company, so my job is to work on different projects of our clients and help them to solve their business problems in the best possible way',
     date: '11.2021 - present',
   },
   {
     title: 'Full stack developer at DevPuzzle',
     description:
-      'Started my corporate career here, worked in a small team with talanted and more experienced developers. My job was to create front end and back end of educational platform.',
+      'Started my corporate career here, worked in a small team with talanted and more experienced developers on an ousource project for our client from Israel. My job was to create front end and back end of the educational platform.',
     date: '06.2021 - 11.2021',
   },
   {
     title: 'Freelance developer',
     description:
-      'Working as a freelance web developer with many clients, mostly dealing with tasks related to CMS, creating websites markup, writing scripts using js and php.',
+      'Worked with a large number of clients as a freelance developer. Engaged in various work on the web,  mostly dealing with tasks related to CMS(especially with Wordpress), creating websites markup, writing scripts using js and php. Helped a lot of people to level up their business.',
     date: '04.2019 - 06.2021',
   },
 ];
