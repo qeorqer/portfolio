71470e9db39b86a19d216aa629e4eb743cda7ca3
george <heohii.pustovit@ava.codes>
2022-11-18 13:05:37 +0200

    add 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,45 +1,16 @@
+import Button from '@components/Button';
+import { HeadFC, navigate } from 'gatsby';
 import * as React from 'react';
-import { Link, HeadFC } from 'gatsby';
-
-const pageStyles = {
-  color: '#232129',
-  padding: '96px',
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4,
-};
 
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === 'development' ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    <main className="text-white text-3xl flex justify-center items-center flex-col h-screen">
+      <p>There is no such page</p>
+      <Button
+        text="Go Home"
+        onClick={() => navigate('/')}
+        customStyles="mt-3 w-28"
+      />
     </main>
   );
 };
