979d2fa7da1cbb365af202154b273c6a4587a49f
george <heohii.pustovit@ava.codes>
2022-11-18 15:08:34 +0200

    remove resize logic

--- src/components/Intro/utils.ts
+++ src/components/Intro/utils.ts
@@ -11,29 +11,29 @@ export const drawCanvas = (canvas: HTMLCanvasElement): void => {
   const values = ['1', '0'];
   const fontSize = 10;
 
-  let columns = canvas.width / fontSize;
-  let drops: number[] = [];
+  const columns = canvas.width / fontSize;
+  const drops: number[] = [];
   for (let x = 0; x < columns; x += 1) {
     drops[x] = 1;
   }
 
-  window.addEventListener('resize', () => {
-    if (
-      canvas.height !== window.innerHeight ||
-      canvas.width !== window.innerWidth
-    ) {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // window.addEventListener('resize', () => {
+  //   if (
+  //     canvas.height !== window.innerHeight ||
+  //     canvas.width !== window.innerWidth
+  //   ) {
+  //     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-      canvas.height = window.innerHeight;
-      canvas.width = window.innerWidth;
+  //     canvas.height = window.innerHeight;
+  //     canvas.width = window.innerWidth;
 
-      columns = canvas.width / fontSize;
-      drops = [];
-      for (let x = 0; x < columns; x += 1) {
-        drops[x] = 1;
-      }
-    }
-  });
+  //     columns = canvas.width / fontSize;
+  //     drops = [];
+  //     for (let x = 0; x < columns; x += 1) {
+  //       drops[x] = 1;
+  //     }
+  //   }
+  // });
 
   const draw = (): void => {
     ctx.fillStyle = 'rgba(17, 24, 39, 0.1)';
