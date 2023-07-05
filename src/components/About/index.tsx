e60159a79a61477bc3eaf8dd86a30c781af3c15b
george <heohii.pustovit@ava.codes>
2023-07-05 09:37:45 +0300

    Update about me

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -20,11 +20,11 @@ const About: React.FC = () => {
         <p className="text-left mt-10 max-w-xl" data-aos="fade">
           For the years spent in this field I had a lot of different
           experiences. I worked with a bunch of small businesses as a freelance
-          developer, participated in creating of a huge learning platform,
-          worked in outsource and outstaff companies, worked in a startup where
-          I had to deal with all stages of development and much more. So I have
-          a solid understanding what the business needs from programmers, and
-          will be happy to apply my skills and knowledge in your projects.
+          developer, worked in outsource and outstaff companies, worked in a
+          startup where I had to deal with all stages of development and much
+          more. So I have a solid understanding what the business needs from
+          programmers, and will be happy to apply my skills and knowledge in
+          your projects.
         </p>
       </div>
     </section>
